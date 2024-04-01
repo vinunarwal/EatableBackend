f4555f65ff9e4c3dab681b19e5e90bbb9c6ed511
vinunarwal <vinunarwal3@gmail.com>
2024-04-01 11:19:48 -0500

    Repoistory Created

---
+++ config/dbConnection.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const conn = async () => {
+    try {
+        const connect = await mongoose.connect("mongodb://127.0.0.1:27017/ProductDetails")
+        console.log("DB Connect",
+            connect.connection.host,
+        );
+    }
+    catch (err) {
+        console.error("DB connection error:", err);
+        process.exit(1);
+    }
+}
+
+module.exports = conn;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 5000;
+
+const dbConnection = require("./config/dbConnection");
+
+app.use(cors());
+app.use(express.json());
+
+dbConnection();
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
